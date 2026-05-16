4a77dbb3e5fffd017ebd7e8891965537e776d99f
hosein <hosein.2006.lavaei@gmail.com>
2026-05-16 21:15:58 +0330

    Added LC004

---
+++ hosein/LC004/__init__.py
@@ -0,0 +1,7 @@
+from .MedianOfTwoSortedArrays import Solution
+from .MedianOfTwoSortedArraysTest import test_median_of_two_sorted_arrays
+
+__all__ = [
+    "Solution",
+    "test_median_of_two_sorted_arrays",
+]
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from amir.LC001 import test_linkedlist_sum as a_lc001
 from hosein.LC001 import test_add_two_numbers as h_lc001
 from hosein.LC002 import test_lswrc as h_lc002
 from hosein.LC003 import test_valid_sudoku as h_lc003
+from hosein.LC004 import test_median_of_two_sorted_arrays as h_lc004
 
 def main():
     hosein_tests()
@@ -11,6 +12,7 @@ def hosein_tests():
     h_lc001()
     h_lc002()
     h_lc003()
+    h_lc004()
 
 def amir_tests():
     a_lc001()
