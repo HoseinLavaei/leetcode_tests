9282ddb8eb55b7b628b06f09092267b06eaa5dfa
hosein <hosein.2006.lavaei@gmail.com>
2026-05-16 21:59:23 +0330

    Added LC005

---
+++ hosein/LC005/ZigzagConversionTest.py
@@ -0,0 +1,5 @@
+from .ZigzagConversion import Solution
+
+def test_zigzag_conversion() -> None:
+    the_string = "PAYPALISHIRING"
+    assert Solution().convert(the_string,3) == "PAHNAPLSIIGYIR"
---
+++ hosein/LC005/__init__.py
@@ -0,0 +1,7 @@
+from .ZigzagConversion import Solution
+from .ZigzagConversionTest import test_zigzag_conversion
+
+__all__ = [
+    "Solution",
+    "test_zigzag_conversion",
+]
