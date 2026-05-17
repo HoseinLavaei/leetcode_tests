574dbfabf1811d3242903f1307a87e63e95481ee
hosein <hosein.2006.lavaei@gmail.com>
2026-05-17 22:35:55 +0330

    added LC008

---
+++ hosein/LC008/ThreeSumTest.py
@@ -0,0 +1,4 @@
+from .ThreeSum import Solution
+
+def test_three_sum():
+    assert Solution().threeSum([-1,0,1,2,-1,-4]) == [[-1,-1,2],[-1,0,1]]
---
+++ hosein/LC008/__init__.py
@@ -0,0 +1,7 @@
+from .ThreeSum import Solution
+from .ThreeSumTest import test_three_sum
+
+__all__ = [
+    "Solution",
+    "test_three_sum",
+]
