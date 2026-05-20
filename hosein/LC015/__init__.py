1b62a2a34aa4588e3106d529137533a1c2bb8407
hosein <hosein.2006.lavaei@gmail.com>
2026-05-20 11:49:19 +0330

    added LC015 & 16

---
+++ hosein/LC015/__init__.py
@@ -0,0 +1,7 @@
+from .SwapNodesInPairs import Solution
+from .SwapNodesInPairsTest import test_swap_nodes_in_pairs
+
+__all__ = [
+    "Solution",
+    "test_swap_nodes_in_pairs",
+]
---
+++ hosein/LC016/__init__.py
@@ -0,0 +1,7 @@
+from .ReverseNodesInKGroups import Solution
+from .ReverseNodesInKGroupsTest import test_reverse_nodes_in_k_groups
+
+__all__ = [
+    "Solution",
+    "test_reverse_nodes_in_k_groups",
+]
