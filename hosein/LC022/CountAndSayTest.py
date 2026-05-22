671a460ea361eeeeb71b7aa959a6aa6dfc5d26d1
hosein <hosein.2006.lavaei@gmail.com>
2026-05-22 21:38:46 +0330

    added LC022

---
+++ hosein/LC022/CountAndSayTest.py
@@ -0,0 +1,4 @@
+from .CountAndSay import Solution
+
+def test_count_and_say():
+    assert Solution().countAndSay(4) == "1211"
---
+++ hosein/LC022/__init__.py
@@ -0,0 +1,7 @@
+from .CountAndSay import Solution
+from .CountAndSayTest import test_count_and_say
+
+__all__ = [
+    "Solution",
+    "test_count_and_say",
+]
